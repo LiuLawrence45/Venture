6a7106c5c1676ab3874508bc1027f1ca0d423e6d
LiuLawrence45 <liu.lawrence45@gmail.com>
2024-01-12 04:25:45 -0800

    back up nav bar; change notification ui (plan to get rid of tab bar with notifications / search)

---
+++ Venture/Bars/OldNavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  OldNavigationBar.swift
+//  Venture
+//
+//  Created by Lawrence Liu on 1/12/24.
+//
+
+import SwiftUI
+
+struct OldNavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OldNavigationBar()
+}
