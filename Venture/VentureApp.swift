b04c4fb7a919d0f477483599b5b85932dcbdd4cf
LiuLawrence45 <liu.lawrence45@gmail.com>
2024-01-11 16:26:57 -0800

    Initial Commit

---
+++ Venture/VentureApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentureApp.swift
+//  Venture
+//
+//  Created by Lawrence Liu on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VentureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
