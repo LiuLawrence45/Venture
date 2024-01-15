2577977e93a0851e8eb00b66028963a2da0e9915
LiuLawrence45 <liu.lawrence45@gmail.com>
2024-01-14 16:18:21 -0800

    new profile update.

---
+++ Venture/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Venture
+//
+//  Created by Lawrence Liu on 1/12/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
