4ab58a5dc764c714acb0d76acc097e93b9040a86
LiuLawrence45 <liu.lawrence45@gmail.com>
2024-01-15 04:44:08 -0800

    coded demo profiles in both profileheader and footer.

---
+++ Venture/Example Data/DemoBucketList.swift
@@ -0,0 +1,18 @@
+//
+//  DemoBucketList.swift
+//  Venture
+//
+//  Created by Lawrence Liu on 1/15/24.
+//
+
+import SwiftUI
+
+struct DemoBucketList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DemoBucketList()
+}
