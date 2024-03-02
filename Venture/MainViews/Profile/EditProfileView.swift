a0efa251f947aa3ba008081fef8b6578333e6c6d
LiuLawrence45 <liu.lawrence45@gmail.com>
2024-03-02 03:26:54 -0800

    basic user profile editing

--- Venture/MainViews/Profile/EditProfileView.swift
+++ Venture/MainViews/Profile/EditProfileView.swift
@@ -7,17 +7,92 @@
 
 import SwiftUI
 
+
 struct EditProfileView: View {
-    
-    
     var profile: ProfileModel
+    @State var username = ""
+    @State var fullName = ""
+    @State var website = ""
     
+    @State var isLoading = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Username", text: $username)
+                        .textContentType(.username)
+                        .textInputAutocapitalization(.never)
+                    TextField("Full name", text: $fullName)
+                        .textContentType(.name)
+                    TextField("Website", text: $website)
+                        .textContentType(.URL)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    Button("Update profile") {
+                        updateProfileButtonTapped()
+                    }
+                    .bold()
+                    
+                    if isLoading {
+                        ProgressView()
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+        }
+        .task {
+            await getInitialProfile()
+        }
+    }
+    
+    func getInitialProfile() async {
+        do {
+            let currentUser = try await supabase.auth.session.user
+            
+            let profile: Profile = try await supabase.database
+                .from("profiles")
+                .select()
+                .eq("id", value: currentUser.id)
+                .single()
+                .execute()
+                .value
+            
+            self.username = profile.username ?? ""
+            self.fullName = profile.fullName ?? ""
+            self.website = profile.website ?? ""
+            
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    func updateProfileButtonTapped() {
+        Task {
+            isLoading = true
+            defer { isLoading = false }
+            do {
+                let currentUser = try await supabase.auth.session.user
+                
+                try await supabase.database
+                    .from("profiles")
+                    .update(
+                        UpdateProfileParams(
+                            username: username,
+                            fullName: fullName,
+                            website: website
+                        )
+                    )
+                    .eq("id", value: currentUser.id)
+                    .execute()
+            } catch {
+                debugPrint(error)
+            }
+        }
     }
 }
-
 #Preview {
     EditProfileView(profile: profiles[0])
 }
